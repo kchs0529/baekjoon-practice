fd5b11343015d3f888732abf662c5daab9e91d2b
kchs0529 <130026527+kchs0529@users.noreply.github.com>
2024-05-03 20:21:28 +0900

    [Silver V] Title: 분수찾기, Time: 152 ms, Memory: 16092 KB -BaekjoonHub

--- Java/백준/Silver/1193. 분수찾기/분수찾기.java
+++ Java/백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,37 +1,29 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		int X = in.nextInt();
- 
-		int cross_count = 1, prev_count_sum = 0;
- 
-		while (true) {
-        
-			// 직전 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
-			if (X <= prev_count_sum + cross_count) {	
-				
-				if (cross_count % 2 == 1) {	// 대각선의 개수가 홀수라면 
-					// 분자가 큰 수부터 시작
+import java.io.*;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int cross_count=1;//해당 범위의 대각선 칸 개수(분자와 분모의 합-1)
+        int sum=0; //해당 대각선 직전 대각선까지의 칸의 합
+        //X-sum은 해당 대각선에서 몇번째 칸인지 확인
+        while(true){
+            if(X<=cross_count+sum){
+                if(cross_count%2==1){
+                    // 분자가 큰 수부터 시작
 					// 분자는 대각선상 내의 블럭 개수 - (X 번째 - 직전 대각선까지의 블럭 개수 - 1) 
 					// 분모는 X 번째 - 직전 대각선까지의 블럭 개수
-					System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
-					break;
-				} 
-				
-				else {	// 대각선상의 블럭의 개수가 짝수라면 
-					// 홀수일 때의 출력을 반대로 
-					System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
-					break;
-				}
- 
-			} else {
-				prev_count_sum += cross_count;
-				cross_count++;
-			}
-		}
-	}
+					System.out.print((cross_count - (X - sum - 1)) + "/" + (X - sum));
+                    break;
+                }else{
+                    //분모가 큰 수부터시작
+                    System.out.print((X - sum) + "/" + (cross_count-(X-sum-1)));
+                    break;
+                }
+            }else{
+                sum+=cross_count;
+                cross_count++;
+            }
+            
+        }
+    }
 }
