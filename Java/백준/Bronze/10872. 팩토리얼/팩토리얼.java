1a67c09691d5d19e7d8dcc11b1322cc6c5d68434
kchs0529 <130026527+kchs0529@users.noreply.github.com>
2024-05-27 14:25:16 +0900

    [Bronze III] Title: 팩토리얼, Time: 200 ms, Memory: 17664 KB -BaekjoonHub

--- Java/백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ Java/백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,17 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int fac = 1;
-        if(N == 0){
-            System.out.print(fac);
-        }
-        else{
-            for(int i=1;i<=N;i++){
-                fac*=i;
-            }
-            System.out.print(fac);
+        System.out.print(factorial(N));
+    }
+
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        } else {
+            return n * factorial(n - 1);
         }
     }
 }
